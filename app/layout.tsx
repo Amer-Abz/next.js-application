60764ce2e29792fabd4cc92f2b9e84da4930ca29
Amer-Abz <amer.abozaki@gmail.com>
2025-03-05 17:57:47 +0200

    Themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import ThemeProvider from "@/context/Theme";
+import Navbar from "@/components/navigation/navbar";
 
 
 const inter = localFont({
@@ -29,11 +31,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable}  antialiased`}
       >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <Navbar/>
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
