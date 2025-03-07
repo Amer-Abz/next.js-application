f611a35630f1ad661775a205e56a1227cfe4612c
Amer-Abz <amer.abozaki@gmail.com>
2025-03-07 15:28:27 +0200

    auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,11 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import ThemeProvider from "@/context/Theme";
-import Navbar from "@/components/navigation/navbar";
+import { Toaster } from "sonner";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
+import { ReactNode } from "react";
+
 
 
 const inter = localFont({
@@ -25,21 +29,23 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout =async ({ children,}: {children: ReactNode})=>{
+
+  const session = await auth();
   return (
     <html lang="en" suppressHydrationWarning>
+      <SessionProvider session={session}>
       <body
         className={`${inter.className} ${spaceGrotesk.variable}  antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        <Navbar/>
+        
         {children}
         </ThemeProvider>
+        <Toaster/>
       </body>
+      </SessionProvider>
     </html>
   );
 }
+export default RootLayout
