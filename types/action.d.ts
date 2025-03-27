11a729ec2a5c43852e6c4d9b9809636f4185249e
Amer-Abz <amer.abozaki@gmail.com>
2025-03-27 17:50:21 +0200

    fix attempt #2

--- types/action.d.ts
+++ types/action.d.ts
@@ -33,7 +33,3 @@ interface GetQuestionParams {
 interface GetTagQuestionsParams extends Omit<PaginatedSearchParams, "filter"> {
   tagId: string;
 }
-
-interface IncrementViewsParams{
-  questionId: string;
-}
