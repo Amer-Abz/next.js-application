538bd06c43926b76a391ec4d58f13d141ff6c08c
Amer-Abz <amer.abozaki@gmail.com>
2025-03-28 15:27:56 +0200

    implement create answer action

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -1,19 +1,25 @@
 import TagCard from '@/components/cards/TagCard';
 import Preview from '@/components/editor/Preview';
+import AnswerForm from '@/components/forms/AnswerForm';
 import Metric from '@/components/Metric';
 import UserAvatar from '@/components/UserAvatar';
 import ROUTES from '@/constants/routes';
-import { getQuestion } from '@/lib/actions/question.action';
+import { getQuestion, incrementViews } from '@/lib/actions/question.action';
 import { formatNumber, getTimeStamp } from '@/lib/utils';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
+import { after } from 'next/server';
 import React from 'react'
 
 
 const QuestionDetails = async({params}: RouteParams) => {
   const {id} = await params;
-  const {success, data:question} = await getQuestion({questionId: id})
 
+  const {success, data:question} = await getQuestion({questionId: id})
+  after(async()=>{
+   await incrementViews({questionId:id}); 
+  })
+  
   if(!success || !question) return redirect("/404")
 
   const {author, createdAt, answers, views, tags, content, title} = question;
@@ -22,11 +28,13 @@ const QuestionDetails = async({params}: RouteParams) => {
     <div className='flex-start w-full flex-col'>
       <div className='flex w-full flex-col-reverse justify-between'>
         <div className='flex items-center justify-start gap-1'>
-          <UserAvatar
-            id = {author._id} name={author.name}
-            className='size-[22px]'
-            fallbackClassName='text-[10px]'
-          />
+        <UserAvatar
+              id={author._id}
+              name={author.name}
+              className="size-[22px]"
+              fallbackClassName="text-[10px]"
+              imageUrl={author.image}
+            />
           <Link href = {ROUTES.PROFILE(author._id)}>
           <p className='paragraph-semibold text-dark300_light700'>
             {author.name}
@@ -76,6 +84,10 @@ const QuestionDetails = async({params}: RouteParams) => {
         />
       ))}
     </div>
+
+    <section className='my-5'>
+      <AnswerForm questionId={question._id}/>
+    </section>
     </>
   )
 }
